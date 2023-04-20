f6bee6f8863b81d14d9c4faef7e595f3a5e16dd3
skshig <skshig@gmail.com>
2023-04-20 14:56:14 +0530

    to do list

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
+let list_item = document.getElementById('newTodoInput');
+let btn = document.getElementById('addTodoBtn');
+let input  = document.getElementById('todoList');
+
+function toDoList(){
+    if(list_item.value != ""){
+        let createdLi = document.createElement('li');
+        createdLi.innerHTML= list_item.value;
+        input.appendChild(createdLi);
+        list_item.value = "";
+    }
+    else {
+        alert("Please enter something in the input box");
+    }
+}
 
